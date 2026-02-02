ccc3e90484a1cf514745ee9d348ccf79c173edd9
Vikas Puri <prvks09@gmail.com>
2026-02-02 11:22:24 +0530

    rockpaperscissors init commit

---
+++ app.js
@@ -0,0 +1,83 @@
+  
+      const outcome = ["Rock", "Paper", "Scissors"];
+      const moves = [
+        { name: "Rock", img: "images/rock-emoji.png" },
+        { name: "Paper", img: "images/paper-emoji.png" },
+        { name: "Scissors", img: "images/scissors-emoji.png" }
+      ];
+
+  
+
+      let score = JSON.parse(localStorage.getItem("score")) ?? {
+        wins: 0,
+        losses: 0,
+        ties: 0,
+      };
+      updateScoreElement();
+
+   
+      function result(playersChoice) {
+
+        const computerChoice = Math.floor(Math.random() * 3);
+        let result = "";
+   
+        
+        renderMove(playersChoice, ".player-choice");
+        renderMove(computerChoice, ".computer-choice");
+
+     
+
+        if (playersChoice == computerChoice) {
+          result = "Tie!";
+          score.ties += 1;
+        } else if (
+          (playersChoice == 0 && computerChoice == 2) ||
+          (playersChoice == 1 && computerChoice == 0) ||
+          (playersChoice == 2 && computerChoice == 1)
+        ) {
+          result = "Won!";
+          score.wins++;
+        } else {
+          result = "Lost!";
+          score.losses++;
+        }
+        updateScoreElement();
+        document.querySelector('.js-result').innerHTML = `${result}`;
+        localStorage.setItem("score", JSON.stringify(score));
+      }
+
+      function reset() {
+        score.losses = 0;
+        score.wins = 0;
+        score.ties = 0;
+
+        document.querySelector('.player-choice').innerHTML = '';
+        document.querySelector('.computer-choice').innerHTML = '';
+        document.querySelector('.js-result').innerHTML = ``;
+
+        updateScoreElement();
+        localStorage.setItem("score", JSON.stringify(score));
+      }
+
+      function updateScoreElement() {
+        const winsEl = document.querySelector('#wins');
+        const lossesEl = document.querySelector('#losses');
+        const tiesEl = document.querySelector('#ties');
+
+        if (winsEl && lossesEl && tiesEl) {
+          winsEl.textContent = score.wins;
+          lossesEl.textContent = score.losses;
+          tiesEl.textContent = score.ties;
+        }
+
+
+      }
+
+
+      
+
+      function renderMove(choice, selector) {
+        const move = moves[choice];
+        document.querySelector(selector).innerHTML =
+        `<img src="${move.img}" alt="${move.name}" class="move-icon" />`;
+      }
